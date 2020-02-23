7af506dada421d1d5b900188950635acf6902a78
ironstein0 <riyansh.karani.011235@gmail.com>
2020-02-23 03:08:58 -0800

    added node websocket server

---
+++ wsserver/index.js
@@ -0,0 +1,17 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({
+	port: 8080
+});
+
+wss.on("connection", function connection(ws) {
+	ws.on('message', function incoming(data) {
+		console.log(`received message: ${data}`)
+
+		wss.clients.forEach(function each(client) {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(data);
+			}
+		});
+	});;
+});
